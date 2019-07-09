3ec8db351342b1fb3191cc8fd1485f71d0fd6b34
Unknown <campino.robin@hotmail.fr>
2019-07-09 19:12:04 +0200

    entity article and com added
    dynamique data on article on

---
+++ SnowTricks/src/Controller/ArticleController.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(ArticleRepository $repository)
+    {
+        $articles = $repository->findAllPublishedOrderedByNewest();
+        return $this->render('article/homepage.html.twig', [
+          'articles'=> $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/article/{slug}")
+     */
+    public function show($slug)
+    {
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->find($slug);
+        return $this->render('article/show.html.twig',[
+           'article'=>$article,
+           ]);
+    }
+}
